7c90eb11c2c9e0dbf41cb718578f2d84b00511dd
ivan_winLap <ivan.ea7h@gmail.com>
2023-02-07 18:07:35 +0100

    parse json

--- src/config.clj
+++ src/config.clj
@@ -1,9 +1,11 @@
 (ns config)
 
 (def input-json-url
-  "https://github.com/bioimage-io/model-runner-java/blob/main/src/main/resources/availableDLVersions.json")
+  ;"https://raw.githubusercontent.com/bioimage-io/model-runner-java/main/src/main/resources/availableDLVersions.json"
+ "https://raw.githubusercontent.com/bioimage-io/model-runner-java/1bf3fd683cf670beeab303c8085f7b9d722217a2/src/main/resources/availableDLVersions.json"
+  )
 
 (def valid-names
   {:os      ["windows" "linux" "macosx"]
    :java-os ["Windows 10" "Linux" "MAC OS X"]
-   :engines {"Tensorflow" "Pytorch"}})
+   :engines #{"Tensorflow" "Pytorch"}})
--- src/download_engines.clj
+++ src/download_engines.clj
@@ -1,3 +1,16 @@
-(ns download-engines)
+(ns download-engines
+  (:require config
+            download
+            [cheshire.core :as json]))
 
-(defn gen-folder-name [])
+(defn fetch-json
+  "download the json as a string"
+  []
+  (slurp config/input-json-url))
+
+(defn parse-json
+  "parse the json string"
+  []
+  (json/parse-string (fetch-json) true))
+
+(defn build-folder-name [])
