5b7ad08973b5416fa76ffe471d710c6215de72e0
ESTEVEZ ALBUJA <hestevez@pa.uc3m.es>
2023-03-13 10:03:20 +0100

    showcase entry not found in example

--- example/Main.java
+++ example/Main.java
@@ -9,11 +9,15 @@ public class Main {
         Download d = new Download();
         File f = new File("./out/");
 
+        // What if selected engine is not found?
+        Boolean c = d.downloadEngine(f,"ppytorch", "1.9.1", false);
+        System.out.println("return is " + c);
+
         // Download all the jars in parallel
         Boolean b = d.downloadEngine(f,"pytorch", "1.9.1", true);
+        System.out.println("return is " + b);
 
-        System.out.println("output is " + b);
-
+        // Default engines folder
         System.out.println("default engines folder is " + d.getDefaultEnginesDirectory());
 
         // Download all the jars in parallel, to the default directory
